19ebf6c524904724acbd85cf2bd74726208796f6
Mehdi Bourgeois <mehdi.bourgeois@gmail.com>
2015-12-28 13:54:40 +0100

    Add Route

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,12 +12,36 @@ angular
   .module('triviatravelApp', [
     'ngRoute'
   ])
+  .constant('config', {
+      appName: 'Trivia.travel',
+      apiUrl: 'http://api.url/'
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl',
         controllerAs: 'main'
+      })
+      .when('/cities', {
+        templateUrl: 'views/cities.html',
+        controller: 'CitiesCtrl',
+        controllerAs: 'cities'
+      })
+      .when('/city/:cityID', {
+        templateUrl: 'views/city.html',
+        controller: 'CityCtrl',
+        controllerAs: 'city'
+      })
+      .when('/question/:cityID', {
+        templateUrl: 'views/question.html',
+        controller: 'QuestionCtrl',
+        controllerAs: 'question'
+      })
+      .when('/admin', {
+        templateUrl: 'views/admin.html',
+        controller: 'AdminCtrl',
+        controllerAs: 'admin'
       })
       .otherwise({
         redirectTo: '/'
