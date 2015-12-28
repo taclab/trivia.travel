0d2577e0bd1a43435c3e9d72f5973972690fdb4b
Mehdi Bourgeois <mehdi.bourgeois@gmail.com>
2015-12-28 13:16:30 +0100

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name triviatravelApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the triviatravelApp
+ */
+angular.module('triviatravelApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name triviatravelApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the triviatravelApp
+ */
+angular.module('triviatravelApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
